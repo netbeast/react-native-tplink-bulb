0b4559dbdac5a7db8394c5fd086e70e2cd4fb22e
Luis Pinto <luisfpintos@gmail.com>
2018-02-01 19:43:31 +0100

    Tplink fix (#1)
    * Fixed tplink light bulb discovery
    
    * Fixing temperature and color changes

--- src/lib.js
+++ src/lib.js
@@ -5,6 +5,8 @@ module.exports = class Bulb {
   constructor ({ip, timeout}) {
     this.ip = ip
     this.timeout = timeout || 3500
+    this.socket = dgram.createSocket('udp4')
+    this.isSocketBound = false
   }
 
   /**
@@ -13,69 +15,22 @@ module.exports = class Bulb {
    * @param {string} filter Only return devices with this class, (ie 'IOT.SMARTBULB')
    * @return {EventEmitter} Emit `light` events when lightbulbs are found
    * @example
-```js
-// turn first discovered light off
-const scan = Bulb.scan()
-  .on('light', light => {
-    light.set(false)
-      .then(status => {
-        console.log(status)
-        scan.stop()
-      })
-  })
-```
-   */
-  static scan (filter) {
-    const emitter = new EventEmitter()
-    const client = dgram.createSocket({
-      type: 'udp4',
-      reuseAddr: true
-    })
-    client.bind(9998, undefined, () => {
-    //  client.setBroadcast(true) // TODO: Check this in iOS
-      const msgBuf = Bulb.encrypt(Buffer.from('{"system":{"get_sysinfo":{}}}'))
-      client.send(msgBuf, 0, msgBuf.length, 9999, '255.255.255.255')
-    })
-    client.on('message', (msg, rinfo) => {
-      const decryptedMsg = this.decrypt(msg).toString('ascii')
-      const jsonMsg = JSON.parse(decryptedMsg)
-      const sysinfo = jsonMsg.system.get_sysinfo
-
-      if (filter && sysinfo.mic_type !== filter) {
-        return
-      }
-
-      const light = new Bulb(rinfo.address)
-      light._info = rinfo
-      light._sysinfo = sysinfo
-      light.host = rinfo.address
-      light.port = rinfo.port
-      light.name = sysinfo.alias
-      light.deviceId = sysinfo.deviceId
-
-      emitter.emit('light', light)
-    })
-    emitter.stop = () => client.close()
-    return emitter
-  }
-
   /**
    * Get info about the Bulb
    * @module info
    * @return {Promise} Resolves to info
    * @example
-```js
-// get info about a light
-const light = new Bulb('10.0.0.200')
-light.info()
-  .then(info => {
-    console.log(info)
-  })
-```
    */
   info () {
     return this.send({system: {get_sysinfo: {}}})
       .then(info => info.system.get_sysinfo)
+  }
+
+  destroy () {
+    if (this.isSocketBound) {
+      this.isSocketBound = false
+      this.socket.close()
+    }
   }
 
   /**
@@ -84,40 +39,29 @@ light.info()
    * @param  {Object} msg Message to send to bulb
    * @return {Promise}    Resolves with answer
    * @example
-```js
-const light = new Bulb('10.0.0.200')
-light.send({
-  'smartlife.iot.smartbulb.lightingservice': {
-    'transition_light_state': {
-      'on_off': 1,
-      'transition_period': 0
-    }
-})
-.then(response => {
-  console.log(response)
-})
-.catch(e => console.error(e))
-```
    */
    send (msg) {
      return new Promise((resolve, reject) => {
        if (!this.ip) {
          return reject(new Error('IP not set.'))
        }
-       const client = dgram.createSocket('udp4')
-       const message = this.encrypt(Buffer.from(JSON.stringify(msg)))
-       setTimeout(() => {
-         client.close()
-         return reject(new Error('TP-Link Bulb connection timeout'))
-       }, this.timeout)
-       client.send(message, 0, message.length, 9999, this.ip, (err, bytes) => {
-         if (err) {
-           client.close()
-           return reject(err)
-         }
-         client.on('message', msg => {
-           resolve(JSON.parse(this.decrypt(msg).toString()))
-           client.close()
+       if (this.isSocketBound) return
+       this.socket.bind(undefined, undefined, () => {
+         this.isSocketBound = true
+         const message = this.encrypt(Buffer.from(JSON.stringify(msg)))
+         setTimeout(() => {
+           this.destroy()
+           return reject(new Error('TP-Link Bulb connection timeout'))
+         }, this.timeout)
+         this.socket.send(message, 0, message.length, 9999, this.ip, (err, bytes) => {
+           if (err) {
+             this.destroy()
+             return reject(err)
+           }
+           this.socket.on('message', msg => {
+             this.destroy()
+             resolve(JSON.parse(this.decrypt(msg).toString()))
+           })
          })
        })
      })
@@ -133,22 +77,16 @@ light.send({
    * @example
    * ```js
 // turn a light on
-const light = new Bulb('10.0.0.200')
-light.set(true)
-  .then(status => {
-    console.log(status)
-  })
-  .catch(err => console.error(err))
-```
    */
   set (power = true, transition = 0, options = {}) {
+    let state = {color_temp: 0, ...options}
     const msg = {
       'smartlife.iot.smartbulb.lightingservice': {
         'transition_light_state': {
           'ignore_default': 1,
           'on_off': power ? 1 : 0,
           'transition_period': transition,
-          ...options
+          ...state
         }
       }
     }
@@ -163,15 +101,6 @@ light.set(true)
    * @param  {Number} year  Full year to check: ie 2017
    * @return {Promise}      Resolves to schedule info
    * @example
-```js
-// get the light's schedule for 1/2017
-const light = new Bulb('10.0.0.200')
-light.schedule(1, 2017)
-  .then(schedule => {
-    console.log(schedule)
-  })
-  .catch(e => console.error(e))
-```
    */
   daystat (month, year) {
     const now = new Date()
@@ -186,15 +115,6 @@ light.schedule(1, 2017)
    * @module cloud
    * @return {Promise} Resolves to cloud info
    * @example
-```js
-// get the cloud info for the light
-const light = new Bulb('10.0.0.200')
-light.cloud()
-  .then(info => {
-    console.log(info)
-  })
-  .catch(e => console.error(e))
-```
    */
   cloud () {
     return this.send({'smartlife.iot.common.cloud': {'get_info': {}}})
@@ -206,15 +126,6 @@ light.cloud()
    * @module schedule
    * @return {Promise} Resolves to schedule info
    * @example
-```js
-// get the bulb's schedule
-const light = new Bulb('10.0.0.200')
-light.schedule()
-  .then(schedule => {
-    console.log(schedule)
-  })
-  .catch(e => console.error(e))
-```
    */
   schedule () {
     return this.send({'smartlife.iot.common.schedule': {'get_rules': {}}})
@@ -226,15 +137,6 @@ light.schedule()
    * @module details
    * @return {Promise} Resolves to operational details
    * @example
-```js
-// get some extra details about the light
-const light = new Bulb('10.0.0.200')
-light.details()
-  .then(details => {
-    console.log(details)
-  })
-  .catch(e => console.error(e))
-```
    */
   details () {
     return this.send({'smartlife.iot.smartbulb.lightingservice': {'get_light_details': {}}})
